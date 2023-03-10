bf4295c19eed3b9624559909e307ea907a0431bd
thanhminhspk05 <thanhminhspk05@gmail.com>
2023-03-10 11:03:45 +0700

    create in crud

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index';
+import CRUDService from '../services/CRUDService';
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -13,11 +14,19 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 };
 
-// object: {
-//     key: '',
-//     value: ''
-// }
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+};
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
+};
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 };
